572780b177145d945af8a80f2c0e0f5e54b03b94
Gabriel Sene <sene300@gmail.com>
2023-09-20 12:17:11 -0300

    feat: Add Complete Method for generation password

--- src/main/java/br/com/chainpass/service/ChainService.java
+++ src/main/java/br/com/chainpass/service/ChainService.java
@@ -3,9 +3,7 @@ package br.com.chainpass.service;
 import br.com.chainpass.domain.dto.RequestChainDto;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 @Service
 public class ChainService {
@@ -13,22 +11,35 @@ public class ChainService {
     Random random = new Random();
     List<String>alfabeto = new ArrayList<>();
     List<Integer>numeros = new ArrayList<>();
+
+    List<String>simbolos = new ArrayList<>();
     public void cadastrarPassword(RequestChainDto dados){
 
     }
 
     public String cadastrarPasswordRandom(){
         String senha  = "";
+        //setUps
+        setUpAbcedario();
+        setUpNumeros();
+        setUpSimbolos();
+
+
         //formação dos 4 caracteres
         for(int i = 0; i < 4; i++){
-            setUpAbcedario();
+
             senha += alfabeto.get(random.nextInt(25));
         }
         for(int i = 0; i < 4; i++){
-            setUpNumeros();
+
             senha += numeros.get(random.nextInt(9));
         }
-        return senha;
+
+        for(int i = 0; i < 4; i++){
+            senha +=  simbolos.get(random.nextInt(3));
+
+        }
+        return senha = embaralhadorDeStrings(senha);
     }
 
     public void setUpAbcedario(){
@@ -43,6 +54,30 @@ public class ChainService {
         for(int i = 0; i < 10; i++){
             numeros.add(i);
         }
+
+    }
+
+    public void setUpSimbolos(){
+        String[] simbolosArray = {"@", "#", "&", "*"};
+        simbolos.addAll(Arrays.asList(simbolosArray));
+    }
+
+    public String embaralhadorDeStrings(String senha){
+        char[] caracteres = senha.toCharArray();
+        String senhaPronta ="";
+
+        List<String>caracteresList = new ArrayList<>();
+        for(char caractere : caracteres){
+            caracteresList.add(String.valueOf(caractere));
+        }
+
+        Collections.shuffle(caracteresList);
+
+        for(int i = 0; i < caracteresList.size(); i++){
+            senhaPronta += caracteresList.get(i);
+
+        }
+        return senhaPronta;
 
     }
 
